9ae1495aa745149d8a7a4732fad0e3a9d49562d8
Shane Hudson <shane@shanehudson.net>
2013-03-27 02:14:39 +0000

    Removed body.js
    You already have html.js, was bugging me that there were two… and not
    being used at all.

--- js/nav-patterns/left-nav-flyout.js
+++ js/nav-patterns/left-nav-flyout.js
@@ -3,8 +3,6 @@
 // No need to thouch this block of code, only the initialisation line below
 var LeftNavFlyout = {
 	init: function(menu, menuLink, wrap, toggleClass) {
-		$('body').addClass('js');
-
 		var $menu = $(menu),
 		$menulink = $(menuLink),
 		$wrap = $(wrap);
